2ee660e437bec2dd6ada1aeeb3ce08a775cd16ea
Ifeoluwa Omoniyi <codetig@outlook.com>
2015-01-25 17:52:41 -0800

    finished basic HW

--- app1.js
+++ app1.js
@@ -9,8 +9,8 @@
 	};
 
 	this.Lights = function(){
-		this.$prestage = document.querySelectorAll(".circle1")[0];
-		this.$stage = document.querySelectorAll(".circle1")[1];
+		this.$prestage = document.querySelectorAll(".circle1");
+		this.$stage = document.querySelectorAll(".two");
 		this.$ready = document.querySelectorAll(".ready");
 		this.$go = document.querySelectorAll(".go");
 		this.$violation = document.querySelectorAll(".disq");
@@ -18,6 +18,15 @@
 	};
 
 	this.Track = function(){
+		this.$raceAreasWidth = document.getElementById("areas").clientWidth;
+		this.realTrackLength = 402; //A drag race trace is a quarter mile or 402m
+		
+		this.$stagingLine1 = Math.round(this.$raceAreasWidth * 0.06);
+		this.$stagingLine2 = Math.round(this.$raceAreasWidth * 0.115);
+		this.$finishLine = Math.round(this.$raceAreasWidth * 0.93);
+		this.trackWidth = this.$finishLine - this.$stagingLine2;
+		//this.$trackWidth = document.getElementById("track").clientWidth; //does not match my calculation
+		this.$racer = document.getElementById("racer");
 		this.$element = "";
 		this.seeFlags = false;
 	};
@@ -30,12 +39,21 @@
 	};
 
 	this.Game = function(){
-		this.raceareas = document.getElementByID('areas');
-		this.racer1 = new Racer(document.getElementByID('racer'));
+		//this.raceareas = document.getElementByID('areas');
+		this.racer1 = new Racer(document.getElementById('racer'));
 		this.lights = new Lights();
 		this.track = new Track();
+		
+		this.racer1.move();
+
+		//turnOn(h.$ready, "yellow", 0, h.$go);
 		//this.timeBoard = new Time();
+
 	};
+
+
+
+
 
 //Object methods
 	Racer.prototype.move = function(){
@@ -46,29 +64,50 @@
 		window.addEventListener("keydown", function(event){
 			console.log(event.keyCode);
 			// console.log(this.offset());
-			if(event.keyCode === 39){
-				self.$element.style.left = (parseFloat(self.$element.style.left) + 0.2) + "%";
-			}
-			if(event.keyCode === 37){
-				self.$element.style.left = (parseFloat(self.$element.style.left) - 0.2) + "%";
+			if(event.keyCode === 83){self.engineOn = true;} //when 's' is pressed, engine is turned on.
+
+			if(self.engineOn){
+				if(event.keyCode === 39){
+					self.$element.style.left = (parseFloat(self.$element.style.left) + 0.2) + "%";
+					if(self.racerTip(self.$element, game.track.$raceAreasWidth) >= game.track.$stagingLine1+2){
+						for(var i = 0; i < game.lights.$stage.length; i++){
+						game.lights.$stage[i].style.backgroundColor = "yellow";
+						}
+						game.lights.turnOn(game.lights.$ready, 0, game.lights.$go, game.lights.$violation);
+					}
+				}
+				if(event.keyCode === 37){
+					self.$element.style.left = (parseFloat(self.$element.style.left) - 0.2) + "%";
+					if(self.racerTip(self.$element, game.track.$raceAreasWidth) <= game.track.$stagingLine1){
+						for(var j = 0; j < game.lights.$stage.length; j++){
+						game.lights.$stage[j].style.backgroundColor = "white";
+						}
+					}
+				}
 			}
 		});
 	};
 
-	Lights.prototype.turnOn = function(lightType, color, p, cb){
+	Lights.prototype.turnOn = function(lightType, p, goLight, violation){
 			var self = this;
 
 			if(p < lightType.length){
 			setTimeout(function(){
-				lightType[p].style.backgroundColor = color;
-				lightType[p+1].style.backgroundColor = color;
-				self.turnOn(lightType, color, p +2, cb);
-			}, 1500);
-			} else{
+				lightType[p].style.backgroundColor = "yellow";
+				lightType[p+1].style.backgroundColor = "yellow";
+				self.turnOn(lightType, p +2, goLight, violation);
+			}, 500);
+			} else if(game.racer1.racerTip(game.racer1.$element, game.track.$raceAreasWidth) <= game.track.$stagingLine2){
 				setTimeout(function(){
-				cb[0].style.backgroundColor = "green";
-				cb[1].style.backgroundColor = "green";	
-				}, 1500);
+				goLight[0].style.backgroundColor = "green";
+				goLight[1].style.backgroundColor = "green";	
+				}, 500);
+			} else if (goLight[0].style.backgroundColor != "green" && game.racer1.racerTip(game.racer1.$element, game.track.$raceAreasWidth) > game.track.$stagingLine2) {
+				console.log(game.racer1.racerTip(game.racer1.$element, game.track.$raceAreasWidth));
+				console.log(game.racer1.$element.style.left);
+
+				violation[0].style.backgroundColor = "red";
+				violation[1].style.backgroundColor = "red";
 			}
 			
 	
@@ -79,21 +118,28 @@
 		//this.prestage
 	};
 
+
+	Racer.prototype.racerTip = function (racer, areaWidth) { //Separated to accomodate 2 racers
+			return racer.clientWidth + ((parseFloat(racer.style.left)/100) * areaWidth);
+		};
+
 	// Game.prototype.attachListeners = function(){
 	// 	var self = this
 	// };
 
 //for testing, will be deleted before game launch.
-	var g = new Racer(document.querySelector("#racer"));
-	g.move();
-	var h = new Lights();
-	console.log(h.$ready);
+	var game = new Game();
+	// g.move();
+	// var h = new Lights();
+	// console.log(h.$ready);
 
-	h.turnOn(h.$ready, "yellow", 0, h.$go);
+	// h.turnOn(h.$ready, "yellow", 0, h.$go);
 	
-	console.log(j.raceArea.clientWidth);
-	//console.log(Stats.pageWidth);
-	console.log(j.stagingLine.style.left);
-
+	// var j = new Stats();
+	// console.log(j.trackWidth);
+	// if(j.racerTip(j.$racer, j.$raceAreasWidth) >= j.$stagingLine2){
+	// 	console.log("yup, tracking racer!");
+	// }
+	
 
 	})();
